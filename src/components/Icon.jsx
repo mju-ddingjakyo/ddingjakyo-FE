2a357b4dea1958686c2135ad5e4e078720d3d91a
qwer0114 <skyok203@naver.com>
2023-11-06 00:43:57 +0900

    refactor : Icon 컴포넌트 사이즈 변경 가능

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-export default function Icon({ iconName }) {
+export default function Icon({ iconName, width }) {
   return (
     <img
       src={process.env.PUBLIC_URL + `/assets/${iconName}.svg`}
       alt={"이미지"}
+      className={width}
     ></img>
   );
 }
