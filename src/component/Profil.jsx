235cdf357dade3639b3d59bf6e3216be83a9203d
‘ujinsimSS’ <syj11243@naver.com>
2023-11-06 19:04:18 +0900

    Feature:프로필 페이지 제작

---
+++ src/component/Profil.jsx
@@ -0,0 +1,317 @@
+function Profil() {
+  return (
+    <div>
+      <div className="w-[1479.11px] h-[832px] relative overflow-hidden bg-[#f9fafd]">
+        <div className="w-[15.41px] h-[15.41px] absolute left-[194.13px] top-[301.99px] overflow-hidden bg-white" />
+        <div className="w-[486.87px] h-[832px] absolute left-[495.35px] top-[-0.77px] bg-white" />
+        <div className="w-[486.87px] h-[62.4px] absolute left-[495.35px] top-[-0.77px] bg-[#432da2]" />
+        <div className="w-[122.49px] h-[36.21px]">
+          <p className="absolute left-[560.83px] top-[13.1px] text-[27.733333587646484px] font-bold text-left text-white">
+            띵작교
+          </p>
+          <img
+            src="스크린샷_2023-11-04_s-removebg-preview-2.png"
+            className="w-[35.44px] h-[34.67px] absolute left-[515.76px] top-[13.48px] object-cover"
+          />
+        </div>
+        <div className="w-[390.12px] h-[524px]">
+          <div
+            className="w-[390.12px] h-[524px] absolute left-[544.64px] top-[153.64px] rounded-[18.03px] bg-white"
+            style={{
+              boxShadow:
+                "0px 2.885427236557007px 14.427135467529297px 0 rgba(0,0,0,0.2)",
+            }}
+          />
+          <img
+            className="absolute left-[677.06px] top-[213.37px]"
+            src="ellipse-16.png"
+          />
+        </div>
+        <p className="w-[225.82px] h-[73.3px] absolute left-[669px] top-[174px] text-[21.965147018432617px] text-left text-black">
+          새 프로필 생성{" "}
+        </p>
+        <p className="w-[161.74px] h-[46.73px] absolute left-[595.65px] top-[389.69px] text-[17.97148323059082px] text-left text-black">
+          이름{" "}
+        </p>
+        <p className="w-[161.74px] h-[46.73px] absolute left-[591.41px] top-[477.53px] text-[17.97148323059082px] text-left text-black">
+          소개글{" "}
+        </p>
+        <p className="w-[161.74px] h-[46.73px] absolute left-[595.65px] top-[524.17px] text-[17.97148323059082px] text-left text-black">
+          나이
+        </p>
+        <p className="w-[161.74px] h-[46.73px] absolute left-[595.65px] top-[570.82px] text-[17.97148323059082px] text-left text-black">
+          MBTI
+        </p>
+        <p className="w-[161.74px] h-[46.73px] absolute left-[596.86px] top-[430.88px] text-[17.97148323059082px] text-left text-black">
+          학과
+        </p>
+        <p className="w-[195.06px] h-[18.17px] absolute left-[676.83px] top-[436.34px] text-[11.697973251342773px] font-medium text-left text-[#b8b8b8]">
+          자신의 학과 Ex. 데이터 테크놀로지 학과
+        </p>
+        <p className="w-[208.39px] h-[37.56px] absolute left-[678.04px] top-[527.2px] text-[11.697973251342773px] font-medium text-left text-[#b8b8b8]">
+          자신의 나이 Ex. 20
+        </p>
+        <p className="w-[208.39px] h-[37.56px] absolute left-[678.04px] top-[396.35px] text-[11.697973251342773px] font-medium text-left text-[#b8b8b8]">
+          이름
+        </p>
+        <p className="w-[199.3px] h-[37.56px] absolute left-[676.83px] top-[480.56px] text-[11.697973251342773px] text-left text-[#b8b8b8]">
+          자신에 대한 소개
+        </p>
+        <p className="w-[77.65px] h-[4.24px] absolute left-[705.3px] top-[347.89px] text-[7.862524032592773px] text-left text-[#442da3]">
+          프로필 사진 설정{" "}
+        </p>
+        <svg
+          width={111}
+          height={43}
+          viewBox="0 0 111 43"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute left-[680.98px] top-[622.83px]"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M0.671631 10.8202C0.671631 5.13266 5.28232 0.521973 10.9699 0.521973H100.02C105.707 0.521973 110.318 5.13266 110.318 10.8202V32.6283C110.318 38.3159 105.707 42.9266 100.02 42.9266H10.9699C5.28232 42.9266 0.671631 38.3159 0.671631 32.6283V10.8202Z"
+            fill="#ECE8FF"
+          />
+        </svg>
+        <p className="w-[144.69px] h-[41.8px] absolute left-[708.33px] top-[632.61px] text-[16.076478958129883px] text-left">
+          <span className="w-[144.69px] h-[41.8px] text-[16.076478958129883px] text-left text-[#442da3]">
+            생성하기
+          </span>
+          <span className="w-[144.69px] h-[41.8px] text-[16.076478958129883px] text-left text-black">
+            {" "}
+          </span>
+        </p>
+        <div
+          className="w-[19.38px] h-[16.96px] absolute left-[676.14px] top-[573.77px] rounded-[6.06px] bg-[#f7f7f7]"
+          style={{
+            boxShadow:
+              "0px 2.423121213912964px 2.423121213912964px 0 rgba(0,0,0,0.25)",
+          }}
+        />
+        <div
+          className="w-[19.38px] h-[16.96px] absolute left-[741.56px] top-[573.77px] rounded-[6.06px] bg-[#d9d9d9]"
+          style={{
+            boxShadow:
+              "0px 2.423121213912964px 2.423121213912964px 0 rgba(0,0,0,0.25)",
+          }}
+        />
+        <div
+          className="w-[19.38px] h-[16.96px] absolute left-[803.96px] top-[573.77px] rounded-[6.06px] bg-[#f7f7f7]"
+          style={{
+            boxShadow:
+              "0px 2.423121213912964px 2.423121213912964px 0 rgba(0,0,0,0.25)",
+          }}
+        />
+        <div
+          className="w-[19.38px] h-[16.96px] absolute left-[831.22px] top-[573.77px] rounded-[6.06px] bg-[#d9d9d9]"
+          style={{
+            boxShadow:
+              "0px 2.423121213912964px 2.423121213912964px 0 rgba(0,0,0,0.25)",
+          }}
+        />
+        <div
+          className="w-[19.38px] h-[16.96px] absolute left-[862.72px] top-[573.77px] rounded-[6.06px] bg-[#f7f7f7]"
+          style={{
+            boxShadow:
+              "0px 2.423121213912964px 2.423121213912964px 0 rgba(0,0,0,0.25)",
+          }}
+        />
+        <div
+          className="w-[19.38px] h-[16.96px] absolute left-[888.16px] top-[573.77px] rounded-[6.06px] bg-[#d9d9d9]"
+          style={{
+            boxShadow:
+              "0px 2.423121213912964px 2.423121213912964px 0 rgba(0,0,0,0.25)",
+          }}
+        />
+        <div
+          className="w-[19.38px] h-[16.96px] absolute left-[768.82px] top-[573.77px] rounded-[6.06px] bg-[#f7f7f7]"
+          style={{
+            boxShadow:
+              "0px 2.423121213912964px 2.423121213912964px 0 rgba(0,0,0,0.25)",
+          }}
+        />
+
+        <p className="w-[11.25px] h-[9.09px] absolute left-[682.88px] top-[578.09px] text-[8.355695724487305px] font-medium text-left text-[#565656]">
+          E
+        </p>
+        <p className="w-3 h-[9.69px] absolute left-[713.78px] top-[577.48px] text-[8.912741661071777px] font-medium text-left text-[#565656]">
+          I
+        </p>
+        <p className="w-3 h-[9.69px] absolute left-[748.31px] top-[578.09px] text-[8.912742614746094px] font-medium text-left text-[#565656]">
+          N
+        </p>
+        <p className="w-3 h-[9.69px] absolute left-[776.17px] top-[578.09px] text-[8.912742614746094px] font-medium text-left text-[#565656]">
+          S
+        </p>
+        <p className="w-3 h-[9.69px] absolute left-[811.91px] top-[577.48px] text-[8.912742614746094px] font-medium text-left text-[#565656]">
+          T
+        </p>
+        <p className="w-[13.5px] h-[10.9px] absolute left-[838.57px] top-[577.48px] text-[10.026833534240723px] font-medium text-left text-[#565656]">
+          F
+        </p>
+        <p className="w-[12.75px] h-[10.3px] absolute left-[869.46px] top-[577.48px] text-[9.46978759765625px] font-medium text-left text-[#565656]">
+          J
+        </p>
+        <p className="w-[12.75px] h-[10.3px] absolute left-[895.51px] top-[577.48px] text-[9.46978759765625px] font-medium text-left text-[#565656]">
+          P
+        </p>
+        <div className="w-[390.12px] h-[524px]">
+          <div
+            className="w-[390.12px] h-[524px] absolute left-[544.64px] top-[153.64px] rounded-[18.03px] bg-white"
+            style={{
+              boxShadow:
+                "0px 2.885427236557007px 14.427135467529297px 0 rgba(0,0,0,0.2)",
+            }}
+          />
+          <img
+            className="absolute left-[677.06px] top-[213.37px]"
+            src="ellipse-16.png"
+          />
+        </div>
+        <p className="w-[225.82px] h-[73.3px] absolute left-[669px] top-[174px] text-[21.965147018432617px] text-left text-black">
+          새 프로필 생성{" "}
+        </p>
+        <p className="w-[161.74px] h-[46.73px] absolute left-[595.65px] top-[389.69px] text-[17.97148323059082px] text-left text-black">
+          이름{" "}
+        </p>
+        <p className="w-[161.74px] h-[46.73px] absolute left-[591.41px] top-[477.53px] text-[17.97148323059082px] text-left text-black">
+          소개글{" "}
+        </p>
+        <p className="w-[161.74px] h-[46.73px] absolute left-[595.65px] top-[524.17px] text-[17.97148323059082px] text-left text-black">
+          나이
+        </p>
+        <p className="w-[161.74px] h-[46.73px] absolute left-[595.65px] top-[570.82px] text-[17.97148323059082px] text-left text-black">
+          MBTI
+        </p>
+        <p className="w-[161.74px] h-[46.73px] absolute left-[596.86px] top-[430.88px] text-[17.97148323059082px] text-left text-black">
+          학과
+        </p>
+        <form>
+          <input
+            className="w-[195.06px] h-[40.17px] absolute left-[676.83px] top-[436.34px] text-[11.697973251342773px] font-medium text-left text-[#b8b8b8]"
+            placeholder="자신의 학과 Ex 데이터 테크놀로지 학과"
+          />
+
+          <input
+            className="w-[208.39px] h-[37.56px] absolute left-[678.04px] top-[527.2px] text-[11.697973251342773px] font-medium text-left text-[#b8b8b8]"
+            placeholder="자신의 나이 Ex 20"
+          />
+
+          <input
+            className="w-[208.39px] h-[37.56px] absolute left-[678.04px] top-[396.35px] text-[11.697973251342773px] font-medium text-left text-[#b8b8b8]"
+            placeholder="이름"
+          />
+
+          <input
+            className="w-[199.3px] h-[37.56px] absolute left-[676.83px] top-[480.56px] text-[11.697973251342773px] text-left text-[#b8b8b8]"
+            placeholder="자신에 대한 소개"
+          />
+        </form>
+
+        <button className="w-[77.65px] h-[4.24px] absolute left-[705.3px] top-[347.89px] text-[7.862524032592773px] text-left text-[#442da3]">
+          프로필 사진 설정{" "}
+        </button>
+        <svg
+          width={111}
+          height={43}
+          viewBox="0 0 111 43"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          className="absolute left-[680.98px] top-[622.83px]"
+          preserveAspectRatio="none"
+        >
+          <path
+            d="M0.671631 10.8202C0.671631 5.13266 5.28232 0.521973 10.9699 0.521973H100.02C105.707 0.521973 110.318 5.13266 110.318 10.8202V32.6283C110.318 38.3159 105.707 42.9266 100.02 42.9266H10.9699C5.28232 42.9266 0.671631 38.3159 0.671631 32.6283V10.8202Z"
+            fill="#ECE8FF"
+          />
+        </svg>
+        <p className="w-[144.69px] h-[41.8px] absolute left-[708.33px] top-[632.61px] text-[16.076478958129883px] text-left">
+          <span className="w-[144.69px] h-[41.8px] text-[16.076478958129883px] text-left text-[#442da3]">
+            생성하기
+          </span>
+          <span className="w-[144.69px] h-[41.8px] text-[16.076478958129883px] text-left text-black">
+            {" "}
+          </span>
+        </p>
+        <div
+          className="w-[19.38px] h-[16.96px] absolute left-[676.14px] top-[573.77px] rounded-[6.06px] bg-[#f7f7f7]"
+          style={{
+            boxShadow:
+              "0px 2.423121213912964px 2.423121213912964px 0 rgba(0,0,0,0.25)",
+          }}
+        />
+        <div
+          className="w-[19.38px] h-[16.96px] absolute left-[741.56px] top-[573.77px] rounded-[6.06px] bg-[#f7f7f7]"
+          style={{
+            boxShadow:
+              "0px 2.423121213912964px 2.423121213912964px 0 rgba(0,0,0,0.25)",
+          }}
+        />
+        <div
+          className="w-[19.38px] h-[16.96px] absolute left-[803.96px] top-[573.77px] rounded-[6.06px] bg-[#f7f7f7]"
+          style={{
+            boxShadow:
+              "0px 2.423121213912964px 2.423121213912964px 0 rgba(0,0,0,0.25)",
+          }}
+        />
+        <div
+          className="w-[19.38px] h-[16.96px] absolute left-[831.22px] top-[573.77px] rounded-[6.06px] bg-[#f7f7f7]"
+          style={{
+            boxShadow:
+              "0px 2.423121213912964px 2.423121213912964px 0 rgba(0,0,0,0.25)",
+          }}
+        />
+        <div
+          className="w-[19.38px] h-[16.96px] absolute left-[862.72px] top-[573.77px] rounded-[6.06px] bg-[#f7f7f7]"
+          style={{
+            boxShadow:
+              "0px 2.423121213912964px 2.423121213912964px 0 rgba(0,0,0,0.25)",
+          }}
+        />
+        <div
+          className="w-[19.38px] h-[16.96px] absolute left-[888.16px] top-[573.77px] rounded-[6.06px] bg-[#f7f7f7]"
+          style={{
+            boxShadow:
+              "0px 2.423121213912964px 2.423121213912964px 0 rgba(0,0,0,0.25)",
+          }}
+        />
+        <div
+          className="w-[19.38px] h-[16.96px] absolute left-[768.82px] top-[573.77px] rounded-[6.06px] bg-[#f7f7f7]"
+          style={{
+            boxShadow:
+              "0px 2.423121213912964px 2.423121213912964px 0 rgba(0,0,0,0.25)",
+          }}
+        />
+
+        <button className="w-[11.25px] h-[9.09px] absolute left-[682.88px] top-[578.09px] text-[8.355695724487305px] font-medium text-left text-[#565656]">
+          E
+        </button>
+        <button className="w-3 h-[9.69px] absolute left-[713.78px] top-[577.48px] text-[8.912741661071777px] font-medium text-left text-[#565656]">
+          I
+        </button>
+        <button className="w-3 h-[9.69px] absolute left-[748.31px] top-[578.09px] text-[8.912742614746094px] font-medium text-left text-[#565656]">
+          N
+        </button>
+        <button className="w-3 h-[9.69px] absolute left-[776.17px] top-[578.09px] text-[8.912742614746094px] font-medium text-left text-[#565656]">
+          S
+        </button>
+        <button className="w-3 h-[9.69px] absolute left-[811.91px] top-[577.48px] text-[8.912742614746094px] font-medium text-left text-[#565656]">
+          T
+        </button>
+        <button className="w-[13.5px] h-[10.9px] absolute left-[838.57px] top-[577.48px] text-[10.026833534240723px] font-medium text-left text-[#565656]">
+          F
+        </button>
+        <button className="w-[12.75px] h-[10.3px] absolute left-[869.46px] top-[577.48px] text-[9.46978759765625px] font-medium text-left text-[#565656]">
+          J
+        </button>
+        <button className="w-[12.75px] h-[10.3px] absolute left-[895.51px] top-[577.48px] text-[9.46978759765625px] font-medium text-left text-[#565656]">
+          P
+        </button>
+      </div>
+      ;
+    </div>
+  );
+}
+
+export default Profil;
