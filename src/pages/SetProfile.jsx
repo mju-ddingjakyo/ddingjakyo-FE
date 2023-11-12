3ca1239e9ac839f9eb0fe2cbd085445bbd970c6c
qwer0114 <skyok203@naver.com>
2023-11-12 16:04:25 +0900

    feat : SetProfile 기능 추가

--- src/pages/SetProfile.jsx
+++ src/pages/SetProfile.jsx
@@ -1,10 +1,13 @@
-import Icon from "../components/Icon";
 import Input from "../components/Input";
-import IconButton from "../components/IconButton";
 import useForm from "../customHook/useForm";
 import validateInput from "../utility/validateInput";
 import MBTICheck from "../components/MBTICheck";
 import React, { useState } from "react";
+import Header from "../components/Header";
+import NumberInput from "../components/NumberInput";
+import Icon from "../components/Icon";
+import profile from "../assets/profile.png";
+import IconButton from "../components/IconButton";
 
 export default function SetProfile() {
   const { onChange, values, errors, handleSubmit } = useForm(
@@ -16,100 +19,102 @@ export default function SetProfile() {
     },
     validateInput
   );
-  const [mbti, setMBTI] = useState(0);
+  const [mbti, setMBTI] = useState("ESTP");
+  const [number, setNumber] = useState(20);
+  const [image, setImage] = useState(profile);
+
+  const uploadImage = (e) => {
+    if (e.target.files[0]) {
+      setImage(e.target.files[0]);
+    } else {
+      setImage(image);
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      if (reader.readyState === 2) {
+        setImage(reader.result);
+      }
+    };
+    reader.readAsDataURL(e.target.files[0]);
+  };
 
   return (
-    <div className="flex flex-col items-center bg-[#f9fafd]">
-      <div className="w-[632px] h-screen bg-white">
-        <div className="w-[632px] h-[81px] bg-[#432da2]">
-          <IconButton className="ml-[17px] pt-[17px] flex flex-wrap">
-            <Icon iconName="logo" width="40" />
-            <div className="ml-[10px] mt-[2px] text-4xl text-left text-white">
-              띵작교
-            </div>
-          </IconButton>
-        </div>
-
-        <div className="flex flex-col items-center">
-          <div
-            className="pt-[5px]  w-[511px] h-[780px] absolute top-[112.5px] rounded-[27px] flex flex-col items-center bg-white"
-            style={{ boxShadow: "0px 4px 20px 0 rgba(0,0,0,0.2)" }}
+    <div>
+      <Header />
+      <div className="flex justify-center">
+        <div className="mt-3 px-14 py-5 rounded-[27px] shadow-2xl">
+          <form
+            className="flex flex-col items-center"
+            onSubmit={handleSubmit}
+            noValidate
           >
             <p className="text-xl pt-[10px] ">새 프로필 생성</p>
-            <svg
-              className="pt-[5px]"
-              width={100}
-              height={150}
-              viewBox="0 0 385 365"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="none"
-            >
-              <ellipse
-                cx="192.5"
-                cy="182.5"
-                rx="192.5"
-                ry="182.5"
-                fill="#D9D9D9"
-              />
-            </svg>
 
-            <input type="file" className="display- hidden" />
+            <input
+              type="file"
+              className="hidden"
+              accept="image/gif, image/jpeg, image/png"
+              id="profilePictureInput"
+              onChange={uploadImage}
+            />
             <label
               htmlFor="profilePictureInput"
               className="cursor-pointer text-xs text-left pt-[10px] text-[#442da3]"
             >
-              프로필 사진설정
+              <img
+                src={image}
+                alt="프로필 이미지"
+                className="rounded-full aspect-square w-[150px]"
+              ></img>
             </label>
+            <Input
+              labelText={"이름"}
+              type={"name"}
+              name={"name"}
+              placeHolder={"이름을 입력하세요"}
+              onChange={onChange}
+              value={values.name}
+              errorMessage={errors.name}
+            />
 
-            <form className="w-500px " onSubmit={handleSubmit} noValidate>
-              <div className="flex flex-col ">
-                <Input
-                  labelText={"이름"}
-                  type={"name"}
-                  name={"name"}
-                  placeHolder={"이름을 입력하세요"}
-                  onChange={onChange}
-                  value={values.name}
-                  errorMessage={errors.name}
-                />
-              </div>
-              <Input
-                labelText={"학과"}
-                type={"major"}
-                name={"major"}
-                placeHolder={"학과를 입력하세요"}
-                onChange={onChange}
-                value={values.major}
-                errorMessage={errors.major}
-              />
-              <Input
-                labelText={"소개글"}
-                type={"selfIntro"}
-                name={"selfIntro"}
-                placeHolder={"소개글을 입력하세요"}
-                onChange={onChange}
-                value={values.selfIntro}
-                errorMessage={errors.selfIntro}
-              />
+            <Input
+              labelText={"학과"}
+              type={"major"}
+              name={"major"}
+              placeHolder={"학과를 입력하세요"}
+              onChange={onChange}
+              value={values.major}
+              errorMessage={errors.major}
+            />
+            <Input
+              labelText={"소개글"}
+              type={"selfIntro"}
+              name={"selfIntro"}
+              placeHolder={"소개글을 입력하세요"}
+              onChange={onChange}
+              value={values.selfIntro}
+              errorMessage={errors.selfIntro}
+            />
+            <div className="w-full flex items-center justify-between mb-10">
+              <div className="text-black text-xl font-bold">나이</div>
+              <NumberInput
+                number={number}
+                setNumber={setNumber}
+                maxNumber={30}
+                minNumber={20}
+              ></NumberInput>
+            </div>
+            <MBTICheck mbti={mbti} setMBTI={setMBTI} />
 
-              <Input
-                labelText={"나이"}
-                type={"age"}
-                name={"age"}
-                placeHolder={"나이를 입력하세요"}
-                onChange={onChange}
-                value={values.age}
-                errorMessage={errors.age}
-              />
-
-              <MBTICheck setMBTI={setMBTI} />
-
-              <button className="w-[186px]  px-3 py-2 mt-6 text-xl font-medium ml-[100px] mb-[20px] text-[#442da3] bg-#ECE8FF rounded-lg hover:bg-purple-300 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-purple-200 dark:hover:bg-purple-300 dark:focus:ring-purple-300">
-                생성하기
-              </button>
-            </form>
-          </div>
+            <button
+              type="submit"
+              className="w-[186px] h-14 mt-8 text-white text-xl bg-violet-800 rounded-lg p-2 hover:bg-violet-400"
+            >
+              생성하기
+            </button>
+          </form>
         </div>
       </div>
     </div>
