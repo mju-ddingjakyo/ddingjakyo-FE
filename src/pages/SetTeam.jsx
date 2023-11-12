d0e56f61b272798ec1355eac7f68e80dd4a88a10
qwer0114 <skyok203@naver.com>
2023-11-12 16:05:27 +0900

    refator: 구조 수정

--- src/pages/SetTeam.jsx
+++ src/pages/SetTeam.jsx
@@ -5,7 +5,7 @@ import React, { useState } from "react";
 import NumberInput from "../components/NumberInput";
 import validateInput from "../utility/validateInput";
 
-export default function SetTeam() {
+export default function SetTeam(closeModal) {
   const { onChange, values, errors, handleSubmit } = useForm(
     {
       teamName: "",
@@ -19,66 +19,70 @@ export default function SetTeam() {
 
   return (
     <div className="flex flex-col items-center ">
-      <div className="w-[632px] h-screen bg-white flex flex-col items-center ">
-        <div
-          className="pt-[10px] w-[511px] h-[700px] absolute  top-[50px] rounded-[27px] flex flex-col items-center bg-white"
-          style={{ boxShadow: "0px 4px 20px 0 rgba(0,0,0,0.2)" }}
+      <div
+        className="pt-[10px] w-[511px] h-[700px] absolute  top-[50px] rounded-[27px] flex flex-col items-center bg-white"
+        style={{ boxShadow: "0px 4px 20px 0 rgba(0,0,0,0.2)" }}
+      >
+        <form
+          className="p-5 flex flex-col items-center rounded-lg text-xl font-bold mt-4 "
+          onSubmit={handleSubmit}
+          noValidate
         >
-          <form
-            className="p-5 flex flex-col items-center bg-white rounded-lg text-xl font-bold mt-4 "
-            onSubmit={handleSubmit}
-            noValidate
-          >
-            <Input
-              labelText={"팀 이름"}
-              type={"teamName"}
-              name={"teamName"}
-              placeHolder={"팀의 이름을 지어주세요."}
-              onChange={onChange}
-              value={values.email}
-              errorMessage={errors.teamName}
-            />
+          <Input
+            labelText={"팀 이름"}
+            type={"teamName"}
+            name={"teamName"}
+            placeHolder={"팀의 이름을 지어주세요."}
+            onChange={onChange}
+            value={values.email}
+            errorMessage={errors.teamName}
+          />
 
-            <GenderCheck setGender={setGender} />
+          <GenderCheck setGender={setGender} />
 
-            <p className="flex mt-12 w-full ">
-              <p className="text-xl font-bold flex flex-col w-full ">인원</p>
-              <NumberInput
-                setNumber={setNumber}
-                number={number}
-                maxNumber={5}
-                minNumber={2}
-              ></NumberInput>
-            </p>
+          <div className="flex mt-12 w-full ">
+            <div className="text-xl font-bold flex flex-col w-full ">인원</div>
+            <NumberInput
+              setNumber={setNumber}
+              number={number}
+              maxNumber={5}
+              minNumber={2}
+            ></NumberInput>
+          </div>
 
-            <p className="w-full flex flex-col  mt-12 mb-12 ">
-              <label className="text-black pb-2 font-semibold">멤버</label>
-              <button
-                type="button"
-                className=" text-violet-500 text-m bg-violet-200 w-1/3 rounded-lg p-2 hover:bg-violet-100"
-              >
-                + 멤버추가
-              </button>
-            </p>
-
-            <Input
-              labelText={"팀 소개"}
-              type={"teamIntro"}
-              name={"teamIntro"}
-              placeHolder={"팀을 소개해주세요."}
-              onChange={onChange}
-              value={values.teamIntro}
-              errorMessage={errors.teamIntro}
-            />
-
-            <button
-              type="submit"
-              className="w-[186px] h-14 mt-8 text-white text-xl bg-violet-800 rounded-lg p-2 hover:bg-violet-400"
+          <div className="w-full flex flex-col  mt-12 mb-12 ">
+            <label
+              className="text-black pb-2 font-semibold"
+              htmlFor="memberButton"
             >
-              완료
+              멤버
+            </label>
+            <button
+              id="memberButton"
+              type="button"
+              className=" text-violet-500 text-m bg-violet-200 w-1/3 rounded-lg p-2 hover:bg-violet-100"
+            >
+              + 멤버추가
             </button>
-          </form>
-        </div>
+          </div>
+
+          <Input
+            labelText={"팀 소개"}
+            type={"teamIntro"}
+            name={"teamIntro"}
+            placeHolder={"팀을 소개해주세요."}
+            onChange={onChange}
+            value={values.teamIntro}
+            errorMessage={errors.teamIntro}
+          />
+
+          <button
+            type="submit"
+            className="w-[186px] h-14 mt-8 text-white text-xl bg-violet-800 rounded-lg p-2 hover:bg-violet-400"
+          >
+            완료
+          </button>
+        </form>
       </div>
     </div>
   );
