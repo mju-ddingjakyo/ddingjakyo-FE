ba781a0b23692f032b52eec043d26b8a96103ad2
qwer0114 <skyok203@naver.com>
2023-11-12 15:53:10 +0900

    feat : Route 페이지 정의

--- src/App.js
+++ src/App.js
@@ -2,23 +2,28 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Join from "./pages/Join";
 import SetProfile from "./pages/SetProfile";
-import Footer from "./components/Footer";
+import SetTeam from "./pages/CreateTeam";
+import Home from "./pages/Home";
+import MyPage from "./pages/MyPage";
+import Match from "./pages/Match";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<></>} />
-          <Route path="/team/:id" element={<></>} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/join" element={<Join />} />
-          <Route path="/profile" element={<SetProfile />} />
-          <Route path="/myteam" element={<></>} />
-          <Route path="/match" element={<></>} />
-          <Route path="/mypage" element={<></>} />
-        </Routes>
-      </BrowserRouter>
+      <div className="w-[632px] h-screen  m-0 m-auto">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/join" element={<Join />} />
+            <Route path="/team/:id" element={<></>} />
+            <Route path="/profile" element={<SetProfile />} />
+            <Route path="/myteam" element={<SetTeam />} />
+            <Route path="/match" element={<Match />} />
+            <Route path="/mypage" element={<MyPage />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
   );
 }
