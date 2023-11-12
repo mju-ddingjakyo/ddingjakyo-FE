7a63e3b763147fd1af42a6db9b68dcdc1448035e
qwer0114 <skyok203@naver.com>
2023-11-13 01:59:26 +0900

    feat : CreateTeam 컴포넌트 정의

--- src/pages/CreateTeam.jsx
+++ src/pages/CreateTeam.jsx
@@ -12,30 +12,27 @@ export default function CreateTeam() {
   const { visibility, openModal, closeModal } = useModal();
   return (
     <>
-      <div className="">
-        <Header></Header>
-        <div className="flex flex-col items-center pt-[180px]">
-          <Icon iconName={createTeam} width={"w-[100px]"} />
-          <span className="pt-[30px] text-3xl font-bold">
-            아직 팀이 없어요!
+      <Header></Header>
+      <div className="flex flex-col items-center pt-[180px]">
+        <Icon iconName={createTeam} width={"w-[100px]"} />
+        <span className="pt-[30px] text-3xl font-bold">아직 팀이 없어요!</span>
+        <p className="pt-[10px] w-96 text-xl font-medium text-center text-[#878787]">
+          <span className="w-96 text-xl font-medium text-center text-[#878787]">
+            나의 팀을 만들고, 띵작교에서
           </span>
-          <p className="pt-[10px] w-96 text-xl font-medium text-center text-[#878787]">
-            <span className="w-96 text-xl font-medium text-center text-[#878787]">
-              나의 팀을 만들고, 띵작교에서
-            </span>
-            <br />
-            <span className="w-96 text-xl font-medium text-center text-[#878787]">
-              새로운 만남을 경험해보세요.
-            </span>
-          </p>
-          <button
-            onClick={openModal}
-            className="w-[286px] h-14 mt-5 text-violet-800 text-xl bg-violet-100 rounded-lg p-2 hover:bg-violet-200 font-bold"
-          >
-            + 팀 만들기
-          </button>
-        </div>
+          <br />
+          <span className="w-96 text-xl font-medium text-center text-[#878787]">
+            새로운 만남을 경험해보세요.
+          </span>
+        </p>
+        <button
+          onClick={openModal}
+          className="w-[286px] h-14 mt-5 text-violet-800 text-xl bg-violet-100 rounded-lg p-2 hover:bg-violet-200 font-bold"
+        >
+          + 팀 만들기
+        </button>
       </div>
+
       <Modal closeModal={closeModal} visibility={visibility}>
         <SetTeam closeModal={closeModal}></SetTeam>
       </Modal>
