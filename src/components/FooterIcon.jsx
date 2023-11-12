3da5c2089513a21adbd7f897bc8b710ebe60cee8
qwer0114 <skyok203@naver.com>
2023-11-12 15:54:46 +0900

    feat : FooterIcon 컴포넌트 정의

--- src/components/FooterIcon.jsx
+++ src/components/FooterIcon.jsx
@@ -1,23 +1,16 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-export default function FooterIcon({ to, children, active, setActive, text }) {
+
+export default function FooterIcon({ children, isActive, text }) {
   return (
-    <NavLink
-      to={to}
-      className={({ isActive }) => {
-        isActive ? setActive(true) : setActive(false);
-      }}
-    >
-      <div className="flex flex-col items-center">
-        {children}
-        <div
-          className={`text-xl font-medium ${
-            active ? "text-black" : "text-neutral-500"
-          }`}
-        >
-          {text}
-        </div>
+    <div className="flex flex-col items-center">
+      {children}
+      <div
+        className={`text-xl font-medium ${
+          isActive ? "text-black" : "text-neutral-500"
+        }`}
+      >
+        {text}
       </div>
-    </NavLink>
+    </div>
   );
 }
