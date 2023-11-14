aca4afbbe20edbfa6ec1d8d6af531a04a3c05c04
qwer0114 <skyok203@naver.com>
2023-11-14 21:36:49 +0900

    feat : MyTeam 컴포넌트 정의

--- src/pages/MyTeam.jsx
+++ src/pages/MyTeam.jsx
@@ -8,7 +8,7 @@ const data = {
   gender: "0",
   content:
     "Cause I know what you like boy (ah-ah)You're my chemical hype boy (ah-ah)내 지난날들은 눈 뜨면 잊는 꿈",
-  member_count: "3",
+  member_count: "5",
   leader_id: "1",
   match_status: "매칭 가능",
   members: [
@@ -17,7 +17,7 @@ const data = {
       nickname: "구준표",
       gender: "0",
       major: "융합소프트웨어학부",
-      introduction: "안녕하세요. 융소의 자랑 김융소입니다.",
+      introduction: "안녕하세요",
       age: 21,
       mbti: "ISTJ",
       profileImage: "https://via.placeholder.com/90x90",
@@ -42,6 +42,26 @@ const data = {
       mbti: "ENFP",
       profileImage: "https://via.placeholder.com/90x90",
     },
+    {
+      memberId: 3,
+      nickname: "윤지후",
+      gender: "0",
+      major: "융합소프트웨어학부",
+      introduction: "안녕하세요.",
+      age: 20,
+      mbti: "ENFP",
+      profileImage: "https://via.placeholder.com/90x90",
+    },
+    {
+      memberId: 3,
+      nickname: "윤지후",
+      gender: "0",
+      major: "융합소프트웨어학부",
+      introduction: "안녕하세요.",
+      age: 20,
+      mbti: "ENFP",
+      profileImage: "https://via.placeholder.com/90x90",
+    },
   ],
 };
 
@@ -49,25 +69,31 @@ export default function MyTeam() {
   return (
     <>
       <Header />
-      <div className="w-full bg-gradient-to-b from-indigo-800 via-indigo-600 to-violet-400">
-        <div className="flex">
-          {data.members.map((member) => (
-            <div className="mx-3">
-              <img
-                className=" w-24
-            h-24
-            rounded-full"
-                src={member.profileImage}
-              ></img>
-            </div>
-          ))}
+      <div className="relative h-1/2 bg-gradient-to-b from-indigo-800 via-indigo-600 to-violet-400">
+        <div className="p-5 flex flex-col items-center">
+          <div className="flex">
+            {data.members.map((member) => (
+              <div className="mx-3">
+                <img
+                  className="w-24 h-24 rounded-full"
+                  src={member.profileImage}
+                  alt="프로필"
+                ></img>
+              </div>
+            ))}
+          </div>
+          <div className="text-white text-4xl font-bold mt-5 self-start">
+            {data.name}
+          </div>
+          <div className="text-white text-2xl font-bold mt-5 self-start">
+            인원 {data.member_count}명
+          </div>
         </div>
-        <div className="text-white text-4xl font-bold">{data.name}</div>
-        <div className="w-full h-[750px] m-0 m-auto flex flex-col items-center  bg-white rounded-tl-[45px] rounded-tr-[45px] mt-10">
-          <div className="w-4/5 h-1/6 text-zinc-600 text-xl font-medium mt-10 border-b-2 border-zinc-400 p-5">
+        <div className="absolute w-full h-[600px] top-60 bg-slate-50 rounded-tl-[45px] rounded-tr-[45px] p-5">
+          <div className="text-zinc-600 text-xl border-b-2 pb-5 border-zinc-400">
             {data.content}
           </div>
-          <div className="p-5">
+          <div className="h-[500px] overflow-y-auto scrollbar mt-5">
             {data.members.map((member) => (
               <Profile
                 img={member.profileImage}
@@ -81,6 +107,7 @@ export default function MyTeam() {
           </div>
         </div>
       </div>
+
       <Footer />
     </>
   );
