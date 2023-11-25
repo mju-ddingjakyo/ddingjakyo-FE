6434f86f71c797e448b2d3364ab2d2d9b168469a
qwer0114 <skyok203@naver.com>
2023-11-26 03:12:54 +0900

    refactor : useForm 수정

--- src/utility/api.js
+++ src/utility/api.js
@@ -4,37 +4,34 @@ const api = axios.create({
   baseURL: "https://localhost:8080/api",
 });
 
-export async function login(formData) {
-  return await api.post(`/login`, formData);
+export async function login({ formData }) {
+  return await api.post(`/login`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
 }
 
 export async function logout() {
   return await api.post("/logout");
 }
 
-export async function register(email, gender, password) {
-  console.log(email, gender, password);
-  return await api.post("/register", {
-    email: `${email}`,
-    gender: `${gender}`,
-    password: `${password}`,
-  });
+export async function register(registerData) {
+  return await api.post("/register", registerData);
 }
 
 export async function emailCertification(email) {
   return await api.post(`/email_certification?email=${email}`);
 }
 
-export async function emailConfirm(email, authCode) {
-  return await api.post(`/email_certification/confirm`, {
-    email: `${email}`,
-    authCode: `${authCode}`,
-  });
+export async function emailConfirm(confirmData) {
+  return await api.post(`/email_certification/confirm`, confirmData);
 }
 
-export async function createProfile(profileData) {
-  return await api.post("/api/member", profileData, {
+export async function createProfile({ formData, JSESSIONID }) {
+  return await api.post("/api/member", formData, {
     headers: {
+      Authorization: "JSESSIONID " + JSESSIONID,
       "Content-Type": "multipart/form-data",
     },
   });
@@ -44,29 +41,55 @@ export async function getAllTeams() {
   return await api.get("/teams");
 }
 
-export async function getTeamDetail(teamId) {
-  return await api.get(`/team/${teamId}`);
+export async function getTeamDetail({ teamId, JSESSIONID }) {
+  return await api.get(`/team/${teamId}`, {
+    headers: {
+      Authorization: "JSESSIONID " + JSESSIONID,
+    },
+  });
 }
 
-export async function getMyTeam() {
+export async function getMyTeam(JSESSIONID) {
   console.log("getMyTeam API 실행됨");
-  return await api.get(`/team/my`);
+  console.log(JSESSIONID);
+  return await api.get(`/team/my`, {
+    headers: {
+      Authorization: "JSESSIONID " + JSESSIONID,
+    },
+  });
 }
 
-export async function createMyTeam(teamData) {
-  return await api.post("/team", teamData);
+export async function createMyTeam({ teamData, JSESSIONID }) {
+  console.log(teamData);
+  return await api.post("/team", teamData, {
+    headers: {
+      Authorization: "JSESSIONID " + JSESSIONID,
+    },
+  });
 }
 
-export async function updateTeam(teamData) {
-  return await api.post("/team", teamData);
+export async function updateTeam({ teamData, JSESSIONID }) {
+  return await api.post("/team", teamData, {
+    headers: {
+      Authorization: "JSESSIONID " + JSESSIONID,
+    },
+  });
 }
 
-export async function deleteTeam(teamData) {
-  return await api.post("/team", teamData);
+export async function deleteTeam({ teamData, JSESSIONID }) {
+  return await api.post("/team", teamData, {
+    headers: {
+      Authorization: "JSESSIONID " + JSESSIONID,
+    },
+  });
 }
 
-export async function getMy() {
-  return await api.get("/member/my");
+export async function getMy(JSESSIONID) {
+  return await api.get("/member/my", {
+    headers: {
+      Authorization: "JSESSIONID " + JSESSIONID,
+    },
+  });
 }
 
 export async function getMemberByID(id) {
@@ -77,9 +100,10 @@ export async function getMemberByEmail(email) {
   return await api.get(`/member?email=${email}`);
 }
 
-export async function updateProfile(profileData) {
-  return await api.put("/member", profileData, {
+export async function updateProfile({ formData, JSESSIONID }) {
+  return await api.put("/member", formData, {
     headers: {
+      Authorization: "JSESSIONID " + JSESSIONID,
       "Content-Type": "multipart/form-data",
     },
   });
@@ -89,35 +113,51 @@ export async function deleteMember() {
   return await api.delete("/member");
 }
 
-export async function proposal(receiveTeamId, kakaoRoomURL) {
-  return await api.post("/proposal", {
-    receiveTeamId: receiveTeamId,
-    kakaoRoomURL: kakaoRoomURL,
+export async function proposal({ proposalData, JSESSIONID }) {
+  return await api.post("/proposal", proposalData, {
+    headers: {
+      Authorization: "JSESSIONID" + JSESSIONID,
+    },
   });
 }
 
-export async function acceptProposal(sendTeamId, mathchingResult) {
-  return await api.post("/proposal", {
-    sendTeamId: `${sendTeamId}`,
-    mathchingResult: `${mathchingResult}`,
+export async function acceptProposal({ proposalData, JSESSIONID }) {
+  return await api.post("/proposal", proposalData, {
+    headers: {
+      Authorization: "JSESSIONID" + JSESSIONID,
+    },
   });
 }
 
-export async function rejectProposal(sendTeamId, mathchingResult) {
-  return await api.post("/proposal", {
-    sendTeamId: `${sendTeamId}`,
-    mathchingResult: `${mathchingResult}`,
+export async function rejectProposal({ proposalData, JSESSIONID }) {
+  return await api.post("/proposal", proposalData, {
+    headers: {
+      Authorization: "JSESSIONID" + JSESSIONID,
+    },
   });
 }
 
-export async function getSendProposal() {
-  return await api.get(`/proposal/send-proposal`);
+export async function getSendProposal(JSESSIONID) {
+  return await api.get(`/proposal/send-proposal`, {
+    headers: {
+      Authorization: "JSESSIONID " + JSESSIONID,
+    },
+  });
 }
 
-export async function getReceiveProposal() {
-  return await api.get(`/proposal/receive-proposals`);
+export async function getReceiveProposal(JSESSIONID) {
+  return await api.get(`/proposal/receive-proposals`, {
+    headers: {
+      Authorization: "JSESSIONID " + JSESSIONID,
+    },
+  });
 }
 
-export async function getCompleteProposal() {
-  return await api.get(`/proposal/complete-proposal`);
+export async function getCompleteProposal(JSESSIONID) {
+  console.log(JSESSIONID);
+  return await api.get(`/proposal/complete-proposal`, {
+    headers: {
+      Authorization: "JSESSIONID " + JSESSIONID,
+    },
+  });
 }
